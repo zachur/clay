acc8db7604d5c0e93b56af710645a81f0861ab72
zachur <z.j.hurst@gmail.com>
2023-12-02 09:13:09 -0600

    Changes

---
+++ mycode.py
@@ -0,0 +1,23 @@
+import re
+
+file_path = r'dummy.eml'
+
+with open(file_path, 'r') as file:
+    file_lines = file.readlines()
+
+# Compile the regex pattern using re.compile()
+ip_pattern = re.compile(r'\b(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)')
+
+ip_addresses = []
+
+for line in file_lines:
+    matches = ip_pattern.findall(line)
+    ip_addresses.extend(matches)
+
+for ip in ip_addresses:
+    print(ip)
+
+output_file_path = 'ipaddr_out.txt'
+with open(output_file_path, 'w') as out_file:
+    for ip in ip_addresses:
+        out_file.write(ip + '\n')
